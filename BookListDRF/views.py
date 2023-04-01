718aec35d878a2c7e54469ccfc00fdca0436496a
Hammad Bin Sajjad <hammadbinsajjad@outlook.com>
2023-04-02 00:26:10 +0500

    Implemented Single Book View

--- BookListDRF/views.py
+++ BookListDRF/views.py
@@ -7,3 +7,7 @@ from .serializers import BookSerializer
 class BookView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class SingleBookView(generics.RetrieveUpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
