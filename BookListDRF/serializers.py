856bf4903478c46693f78adbe4d37f5b4a9d62e6
Hammad Bin Sajjad <hammadbinsajjad@outlook.com>
2023-04-02 11:48:39 +0500

    Prevent id from being sent in response

--- BookListDRF/serializers.py
+++ BookListDRF/serializers.py
@@ -5,6 +5,6 @@ from rest_framework import serializers
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ["id", "title", "author", "price"]
+        fields = ["title", "author", "price"]
 
     
