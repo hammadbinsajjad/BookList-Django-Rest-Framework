c7134ae15d85e8d138ed2f8807e21361f848c7ea
Hammad Bin Sajjad <hammadbinsajjad@outlook.com>
2023-04-02 13:13:22 +0500

    Added XML response rendering

--- BookListDRF/views.py
+++ BookListDRF/views.py
@@ -1,13 +1,17 @@
 from rest_framework import generics
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework_xml.renderers import XMLRenderer
 from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
 
 # Create your views here.
 class BookView(generics.ListCreateAPIView):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer, XMLRenderer]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class SingleBookView(generics.RetrieveUpdateAPIView):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer, XMLRenderer]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
